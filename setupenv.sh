25a4d72c4a42521e249ebc78ac3dfcf66b11021b
Kevin Stanley-Adams <kevin@stanley-adams.co.uk>
2021-02-01 11:21:36 +0100

    Fixes to GPU env setup

--- setupenv.sh
+++ setupenv.sh
@@ -3,11 +3,13 @@ sudo apt --assume-yes install ffmpeg python3-venv
 
 python3 -m venv venvgen
 source venvgen/bin/activate
+python -m pip install wheel
 python -m pip install -r requirements-latest.txt
 
+python -m pip install requests
 git clone https://github.com/chentinghao/download_google_drive.git
-FILEID="1L8P-hpBhZi8Q_1vP2KlQ4N6dvlzpYBvZ"
-FILEPATH="images/vox-adv-cpk.pth.tar"
+FILEID="1Z1dc_gQSmafDeWgqRxTJwsDhT7C7g8XF"
+FILEPATH="config/vox-adv-cpk.pth.tar"
 python download_google_drive/download_gdrive.py $FILEID $FILEPATH
 
 CUDA_REPO_PKG=cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
