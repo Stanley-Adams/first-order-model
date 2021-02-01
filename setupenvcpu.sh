60cc77e8f63044142a76b11ab41f730bdf14f8c6
Kevin Stanley-Adams <kevin@stanley-adams.co.uk>
2021-02-01 10:21:02 +0100

    gdrive downloader updates

--- setupenvcpu.sh
+++ setupenvcpu.sh
@@ -5,7 +5,8 @@ python3 -m venv venvgen
 source venvgen/bin/activate
 python -m pip install -r requirements-latest.txt
 
+python -m pip install requests
 git clone https://github.com/chentinghao/download_google_drive.git
-FILEID="1L8P-hpBhZi8Q_1vP2KlQ4N6dvlzpYBvZ"
-FILEPATH="images/vox-adv-cpk.pth.tar"
+FILEID="1Z1dc_gQSmafDeWgqRxTJwsDhT7C7g8XF"
+FILEPATH="config/vox-adv-cpk.pth.tar"
 python download_google_drive/download_gdrive.py $FILEID $FILEPATH
