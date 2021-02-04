27ffc2291d28e20fbed52b830d1fbb3e0ec74c7b
Kevin Stanley-Adams <kevin@stanley-adams.co.uk>
2021-02-04 12:13:22 +0100

    added face crop ml tool

--- setupenvcpu.sh
+++ setupenvcpu.sh
@@ -11,3 +11,8 @@ git clone https://github.com/chentinghao/download_google_drive.git
 FILEID="1Z1dc_gQSmafDeWgqRxTJwsDhT7C7g8XF"
 FILEPATH="config/vox-adv-cpk.pth.tar"
 python download_google_drive/download_gdrive.py $FILEID $FILEPATH
+
+git clone https://github.com/Stanley-Adams/face-alignment
+cd face-alignment
+python -m install -r requirements.txt
+python setup.py install
